59535e921f883be52ad9da489cdf4f04293d183c
EzikeChris <synergyaffirm@gmail.com>
2023-09-05 15:04:49 +0100

    chore: remove favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,9 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to savannahtech!</title>
+      <link rel="stylesheet" href="css/styles.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossOrigin="anonymous" referrerPolicy="no-referrer"/>
+    <title>אודות אגף התפעול</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
